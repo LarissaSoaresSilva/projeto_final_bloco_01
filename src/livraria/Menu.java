60abf3ef75a7c955f7c078b13489e8c2699921db
Bowie <larissa.soaress@yahoo.com.br>
2025-04-04 11:38:41 -0300

    Primeira etapa finalizada

--- src/livraria/Menu.java
+++ src/livraria/Menu.java
@@ -1,10 +1,78 @@
 package livraria;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner ler = new Scanner(System.in);
+
+		int opcaoMenu = 0, codigoDoLivro, quantidadeDoLivro;
+		float precoDoLivro;
+		String nomeDoLivro, autorDoLivro, generoDoLivro;
+
+		while (true) {
+
+			System.out.println("#### ### ### ### ### ### ### ### ### ### ####");
+			System.out.println("                                             ");
+			System.out.println("        Livraria Soares Há Um Click          ");
+			System.out.println("                                             ");
+			System.out.println("#### ### ### ### ### ### ### ### ### ### ####");
+			System.out.println("                                             ");
+			System.out.println("     1 - Adicionar o Livro no estoque        ");
+			System.out.println("     2 - Listar todos os Livros do estoque   ");
+			System.out.println("     3 - Alterar dados dos Livros do estoque ");
+			System.out.println("     4 - Excluir Livro do estoque            ");
+			System.out.println("     0 - Sair                                ");
+			System.out.println("                                             ");
+			System.out.println("#### ### ### ### ### ### ### ### ### ### ####");
+			System.out.println("Entre com a opção desejada:                  ");
+			System.out.println("                                             ");
+			opcaoMenu = ler.nextInt();
+
+			if (opcaoMenu == 0) {
+
+				System.out.println(
+						"\nLivraria Soares Há Um Click - Livros ao seu alcance, com um clique de distância! \n");
+				sobre();
+				ler.close();
+				System.exit(0);
+			}
+
+			switch (opcaoMenu) {
+
+			case 1:
+				System.out.println("\nAdicionar o  Livro no estoque\n");
+
+				break;
+
+			case 2:
+				System.out.println("\nListar todos os Livros do estoque: \n");
+				break;
+
+			case 3:
+				System.out.println("\nAlterar dados dos Livros do estoque: \n");
+				break;
+			case 4:
+				System.out.println("\nExcluir Livro do estoque: \n");
+				break;
+
+			default:
+				if (opcaoMenu < 0 || opcaoMenu > 4) {
+					System.out.println("\nOpção Inválida!\n");
+					break;
+				}
+			}
+		}
 	}
 
+	public static void sobre() {
+
+		System.out.println("****************************************************\n");
+		System.out.println("Projeto Desenvolvido por: Larissa Soares da Silva.    ");
+		System.out.println("github.com/LarissaSoaresSilva/projeto_final_bloco_01  ");
+		System.out.println("\n****************************************************");
+
+	}
 }
